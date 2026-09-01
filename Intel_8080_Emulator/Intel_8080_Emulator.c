5ba48e4daba974cb13d719d7e4a1fab36063e1eb
Psalm Francis Caadan <caadanpsalmfrancis.g@gmail.com>
2026-09-01 17:06:05 +0800

    implement: MOV Opcodes

--- Intel_8080_Emulator/Intel_8080_Emulator.c
+++ Intel_8080_Emulator/Intel_8080_Emulator.c
@@ -340,7 +340,7 @@ void UnimplementedInstruction (State8080* state) {
     printf("Error: Unimplemented Instruction.\n");
     exit(1);
 }
-/*
+/*n
     offsetHelper - Helper function that combines the 2 8 bit registers h (high) and l (low) registers into an address in the 16 bit memory.
 
     Explanation:
@@ -439,73 +439,72 @@ int Emulate8080OpCode(State8080* state) {
         case 0x3e:UnimplementedInstruction(state); break;
         case 0x3f:UnimplementedInstruction(state); break;
 
-        case 0x40:UnimplementedInstruction(state); break;
-        case 0x41:UnimplementedInstruction(state); break;
-        case 0x42:UnimplementedInstruction(state); break;
-        case 0x43:UnimplementedInstruction(state); break;
-        case 0x44:UnimplementedInstruction(state); break;
-        case 0x45:UnimplementedInstruction(state); break;
-        case 0x46:UnimplementedInstruction(state); break;
-        case 0x47:UnimplementedInstruction(state); break;
-        case 0x48:UnimplementedInstruction(state); break;
-        case 0x49:UnimplementedInstruction(state); break;
-        case 0x4a:UnimplementedInstruction(state); break;
-        case 0x4b:UnimplementedInstruction(state); break;
-        case 0x4c:UnimplementedInstruction(state); break;
-        case 0x4d:UnimplementedInstruction(state); break;
-        case 0x4e:UnimplementedInstruction(state); break;
-        case 0x4f:UnimplementedInstruction(state); break;
+        case 0x40:state->b = state->b; break;  //MOV B, B
+        case 0x41:state->b = state->c; break;  //MOV B, C
+        case 0x42:state->b = state->d; break;  //MOV B, D
+        case 0x43:state->b = state->e; break;  //MOV B, E
+        case 0x44:state->b = state->h; break;  //MOV B, H
+        case 0x45:state->b = state->l; break;  //MOV B, L
+        case 0x46:state->b = state->memory[offsetHelper(state)]; break;  //MOV B, M 
+        case 0x47:state->b = state->a; break;  //MOV B, A
+        case 0x48:state->c = state->b; break;  //MOV C, B
+        case 0x49:state->c = state->c; break;  //MOV C, C
+        case 0x4a:state->c = state->d; break;  //MOV C, D
+        case 0x4b:state->c = state->e; break;  //MOV C, E
+        case 0x4c:state->c = state->h; break;  //MOV C, H
+        case 0x4d:state->c = state->l; break;  //MOV C, L
+        case 0x4e:state->c = state->memory[offsetHelper(state)]; break;  //MOV C, M
+        case 0x4f:state->c = state->a; break;  //MOV C, A
         
-        case 0x50:UnimplementedInstruction(state); break;
-        case 0x51:UnimplementedInstruction(state); break;
-        case 0x52:UnimplementedInstruction(state); break;
-        case 0x53:UnimplementedInstruction(state); break;
-        case 0x54:UnimplementedInstruction(state); break;
-        case 0x55:UnimplementedInstruction(state); break;
-        case 0x56:UnimplementedInstruction(state); break;
-        case 0x57:UnimplementedInstruction(state); break;
-        case 0x58:UnimplementedInstruction(state); break;
-        case 0x59:UnimplementedInstruction(state); break;
-        case 0x5a:UnimplementedInstruction(state); break;
-        case 0x5b:UnimplementedInstruction(state); break;
-        case 0x5c:UnimplementedInstruction(state); break;
-        case 0x5d:UnimplementedInstruction(state); break;
-        case 0x5e:UnimplementedInstruction(state); break;
-        case 0x5f:UnimplementedInstruction(state); break;
+        case 0x50:state->d = state->b; break;  //MOV D, B
+        case 0x51:state->d = state->c; break;  //MOV D, C
+        case 0x52:state->d = state->d; break;  //MOV D, D
+        case 0x53:state->d = state->e; break;  //MOV D, E
+        case 0x54:state->d = state->h; break;  //MOV D, H
+        case 0x55:state->d = state->l; break;  //MOV D, L
+        case 0x56:state->d = state->memory[offsetHelper(state)]; break;  //MOV D, M
+        case 0x57:state->d = state->a; break;  //MOV D, A
+        case 0x58:state->e = state->b; break;  //MOV E, B
+        case 0x59:state->e = state->c; break;  //MOV E, C
+        case 0x5a:state->e = state->d; break;  //MOV E, D
+        case 0x5b:state->e = state->e; break;  //MOV E, E
+        case 0x5c:state->e = state->h; break;  //MOV E, H
+        case 0x5d:state->e = state->l; break;  //MOV E, L
+        case 0x5e:state->e = state->memory[offsetHelper(state)]; break;  //MOV E, M
+        case 0x5f:state->e = state->a; break;  //MOV E, A
 
-        case 0x60:UnimplementedInstruction(state); break;
-        case 0x61:UnimplementedInstruction(state); break;
-        case 0x62:UnimplementedInstruction(state); break;
-        case 0x63:UnimplementedInstruction(state); break;
-        case 0x64:UnimplementedInstruction(state); break;
-        case 0x65:UnimplementedInstruction(state); break;
-        case 0x66:UnimplementedInstruction(state); break;
-        case 0x67:UnimplementedInstruction(state); break;
-        case 0x68:UnimplementedInstruction(state); break;
-        case 0x69:UnimplementedInstruction(state); break;
-        case 0x6a:UnimplementedInstruction(state); break;
-        case 0x6b:UnimplementedInstruction(state); break;
-        case 0x6c:UnimplementedInstruction(state); break;
-        case 0x6d:UnimplementedInstruction(state); break;
-        case 0x6e:UnimplementedInstruction(state); break;
-        case 0x6f:UnimplementedInstruction(state); break;
+        case 0x60:state->h = state->b; break;  //MOV H, B
+        case 0x61:state->h = state->c; break;  //MOV H, C
+        case 0x62:state->h = state->d; break;  //MOV H, D
+        case 0x63:state->h = state->e; break;  //MOV H, E
+        case 0x64:state->h = state->h; break;  //MOV H, H
+        case 0x65:state->h = state->l; break;  //MOV H, L
+        case 0x66:state->h = state->memory[offsetHelper(state)]; break;  //MOV H, M
+        case 0x68:state->l = state->b; break;  //MOV L, B
+        case 0x69:state->l = state->c; break;  //MOV L, C
+        case 0x6a:state->l = state->d; break;  //MOV L, D
+        case 0x6b:state->l = state->e; break;  //MOV L, E
+        case 0x6c:state->l = state->h; break;  //MOV L, H
+        case 0x6d:state->l = state->l; break;  //MOV L, L
+        case 0x6e:state->l = state->memory[offsetHelper(state)]; break;  //MOV L, M
+        case 0x6f:state->l = state->a; break;  //MOV L, A
         
-        case 0x70:UnimplementedInstruction(state); break;
-        case 0x71:UnimplementedInstruction(state); break;
-        case 0x72:UnimplementedInstruction(state); break;
-        case 0x73:UnimplementedInstruction(state); break;
-        case 0x74:UnimplementedInstruction(state); break;
-        case 0x75:UnimplementedInstruction(state); break;
-        case 0x76:UnimplementedInstruction(state); break;
-        case 0x77:UnimplementedInstruction(state); break;
-        case 0x78:UnimplementedInstruction(state); break;
-        case 0x79:UnimplementedInstruction(state); break;
-        case 0x7a:UnimplementedInstruction(state); break;
-        case 0x7b:UnimplementedInstruction(state); break;
-        case 0x7c:UnimplementedInstruction(state); break;
-        case 0x7d:UnimplementedInstruction(state); break;
-        case 0x7e:UnimplementedInstruction(state); break;
-        case 0x7f:UnimplementedInstruction(state); break;
+        case 0x70:state->d = state->b; break;  //MOV M, B //TODO
+        case 0x71:state->d = state->c; break;  //MOV M, C //TODO
+        case 0x72:state->d = state->d; break;  //MOV M, D //TODO
+        case 0x73:state->d = state->e; break;  //MOV M, E //TODO
+        case 0x74:state->d = state->h; break;  //MOV M, H //TODO
+        case 0x75:state->d = state->l; break;  //MOV M, L //TODO
+        case 0x76:UnimplementedInstruction(state); break; // HLT
+        case 0x77:state->a = state->a; break;  //MOV M, A //TODO
+        case 0x78:state->a = state->b; break;  //MOV A, B
+        case 0x79:state->a = state->c; break;  //MOV A, C
+        case 0x7a:state->a = state->d; break;  //MOV A, D
+        case 0x7b:state->a = state->e; break;  //MOV A, E
+        case 0x7c:state->a = state->h; break;  //MOV A, H
+        case 0x7d:state->a = state->l; break;  //MOV A, L
+        case 0x7e:state->a = state->memory[offsetHelper(state)]; break;  //MOV A, M
+        case 0x7f:state->a = state->a; break;  //MOV A, A
         
         case 0x80:UnimplementedInstruction(state); break;
         case 0x81:UnimplementedInstruction(state); break;
